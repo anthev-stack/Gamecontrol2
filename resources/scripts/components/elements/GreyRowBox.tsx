be56effe0b78f9c08071929b22434a1277890467
anthev <cameronanthev@gmail.com>
2025-10-26 13:38:43 +1000

    Remove bg-neutral-700 directly from GreyRowBox component

--- resources/scripts/components/elements/GreyRowBox.tsx
+++ resources/scripts/components/elements/GreyRowBox.tsx
@@ -2,13 +2,15 @@ import styled from 'styled-components/macro';
 import tw from 'twin.macro';
 
 export default styled.div<{ $hoverable?: boolean }>`
-    ${tw`flex rounded no-underline text-neutral-200 items-center bg-neutral-700 p-4 border border-transparent transition-colors duration-150 overflow-hidden`};
-
+    ${tw`flex rounded no-underline text-neutral-200 items-center p-4 border border-transparent transition-colors duration-150 overflow-hidden`};
+    background: none;
+    
     ${(props) => props.$hoverable !== false && tw`hover:border-neutral-500`};
 
     & .icon {
         ${tw`w-16 flex items-center justify-center p-3`};
-        background: none !important;
-        border: none !important;
+        background: none;
+        border: none;
+        border-radius: 0;
     }
 `;
