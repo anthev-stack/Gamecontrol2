d80a5ae7dab03926fb33140eb673a3ac85c51647
anthev <cameronanthev@gmail.com>
2025-10-26 12:24:27 +1000

    Fix admin panel - restore account and servers routes

--- routes/base.php
+++ routes/base.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use Pterodactyl\Http\Controllers\Base;
 use Pterodactyl\Http\Middleware\RequireTwoFactorAuthentication;
 
+Route::get('/account', [Base\IndexController::class, 'index'])
+    ->withoutMiddleware(RequireTwoFactorAuthentication::class)
+    ->name('account');
+
+Route::get('/servers', [Base\IndexController::class, 'index'])->name('servers');
+
 Route::get('/locales/locale.json', Base\LocaleController::class)
     ->withoutMiddleware(['auth', RequireTwoFactorAuthentication::class])
     ->where('namespace', '.*');
