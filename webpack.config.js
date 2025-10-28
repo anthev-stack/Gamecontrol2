63ff62b7e253a935254486be1351f16a59159cf5
anthev <cameronanthev@gmail.com>
2025-10-29 07:58:09 +1000

    Add babel plugins for optional chaining and nullish coalescing to support @tanstack modules

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,10 @@ module.exports = {
                                 }
                             }]
                         ],
+                        plugins: [
+                            '@babel/plugin-proposal-optional-chaining',
+                            '@babel/plugin-proposal-nullish-coalescing-operator'
+                        ],
                     },
                 },
             },
